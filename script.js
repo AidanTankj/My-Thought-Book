dc0b4e5b8107c9fd12a25b807080c6377170f852
Aidan Tan <kayjien@gmail.com>
2025-09-24 02:59:21 +0800

    Docs: added comments to explain functionality

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+// Get a reference to the form element
 const newEntryForm = document.getElementById('new-entry-form');
 
+// Add an event listener to the form's 'submit' event
 newEntryForm.addEventListener('submit', (event) => {
+    // Prevent the browser from refreshing the page when submit button pressed
     event.preventDefault();
 
+    // Get the values from the form
     const title = document.getElementById('entry-title').value;
     const content = document.getElementById('entry-content').value;
 
+    // Check to ensure non-empty submission
     if (title.trim() === '' && content.trim() === '') {
         console.log('Cannot save an empty entry.');
         return;
